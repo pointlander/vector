94560af55775d41ba787d1d6e84b5f898baa3bb5
Andrew Snodgrass <pointlander@gmail.com>
2024-02-02 21:51:31 -0600

    Added line intersection points

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
+	"sort"
 
 	. "github.com/pointlander/matrix"
 )
@@ -14,14 +16,22 @@ import (
 // Line is a line sample
 type Line struct {
 	B1, B0 float64
-	Cost   float64
+}
+
+// Point is a point
+type Point struct {
+	X    float64
+	Y    float64
+	A    Line
+	B    Line
+	Cost float64
 }
 
 func main() {
 	rng := rand.New(rand.NewSource(1))
 	x := NewZeroMatrix(10, 1)
 	y := NewZeroMatrix(10, 1)
-	samples := make([]Line, 10)
+	samples := make([]Line, 300)
 	for i := range samples {
 		for j := range x.Data {
 			a := rng.Intn(100) + 1
@@ -34,5 +44,36 @@ func main() {
 	}
 	for i := range samples {
 		fmt.Println(samples[i])
+	}
+	fmt.Println()
+	points := make([]Point, 0, 8)
+	for i := 0; i < len(samples); i++ {
+		for j := i + 1; j < len(samples); j++ {
+			x := (samples[j].B0 - samples[i].B0) / (samples[i].B1 - samples[j].B1)
+			y := samples[i].B1*x + samples[i].B0
+			points = append(points, Point{
+				X: x,
+				Y: y,
+				A: samples[i],
+				B: samples[j],
+			})
+		}
+	}
+	for i := range points {
+		fmt.Println(points[i])
+	}
+	for i := 0; i < len(points); i++ {
+		for j := 0; j < len(points); j++ {
+			diffx := points[i].X - points[j].X
+			diffy := points[i].Y - points[j].Y
+			points[i].Cost += math.Sqrt(diffx*diffx + diffy*diffy)
+		}
+	}
+	sort.Slice(points, func(i, j int) bool {
+		return points[i].Cost < points[j].Cost
+	})
+	fmt.Println()
+	for i := range points[:10] {
+		fmt.Println(points[i].X, points[i].Y, points[i].Cost)
 	}
 }
