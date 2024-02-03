c3d8ec097ccd2368ff6ae039bae8465b13e80396
Andrew Snodgrass <pointlander@gmail.com>
2024-02-02 20:19:59 -0600

    Generate line samples

--- main.go
+++ main.go
@@ -6,13 +6,33 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	. "github.com/pointlander/matrix"
 )
 
+// Line is a line sample
+type Line struct {
+	B1, B0 float64
+	Cost   float64
+}
+
 func main() {
-	x := NewMatrix(10, 1, []float32{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}...)
-	y := NewMatrix(10, 1, []float32{1, 3, 2, 5, 7, 8, 8, 9, 10, 12}...)
-	b0, b1 := LinearRegression(x, y)
-	fmt.Println(b0, b1)
+	rng := rand.New(rand.NewSource(1))
+	x := NewZeroMatrix(10, 1)
+	y := NewZeroMatrix(10, 1)
+	samples := make([]Line, 10)
+	for i := range samples {
+		for j := range x.Data {
+			a := rng.Intn(100) + 1
+			x.Data[j] = float32(a)
+			y.Data[j] = float32(77 % a)
+		}
+		b0, b1 := LinearRegression(x, y)
+		samples[i].B0 = b0
+		samples[i].B1 = b1
+	}
+	for i := range samples {
+		fmt.Println(samples[i])
+	}
 }
